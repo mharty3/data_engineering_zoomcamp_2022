0d57b82e9b912e8d46f20e9a5a02b5285fea1c20
Michael Harty <michaelharty3@gmail.com>
2022-01-17 09:28:43 -0700

    Add python script as entrypoint to Dockerfile
    By adding the Python script to the entry point, running the container
    will now execute the python script.
    
    I also added `sys.argv` to the Python script to accept command line
    arguments that can be used to parameterize the pipeline.
    
    There are notes in the readme describing the docker commands

--- week01/docker/pipeline.py
+++ week01/docker/pipeline.py
@@ -1,8 +1,15 @@
 # simple script to stand in for data manipulation with pandas
 # now it simply demonstration that docker is available in the container
+#
+# it uses sys.argv to pass command line arguments to the file in order to 
+# parameterize the data pipeline
 
 import pandas as pd
+import sys
+
+print(sys.argv)
+day = sys.argv[1]
 
 # some fancy stuff with pandas
 
-print('job finished successfully')
+print(f'job finished successfully for day = {day}')
