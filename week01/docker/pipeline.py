18d832645709a414c9310c8e1ed2ba377db9a099
Michael Harty <michaelharty3@gmail.com>
2022-01-17 08:52:00 -0700

    add python script to be executed in the container

---
+++ week01/docker/pipeline.py
@@ -0,0 +1,8 @@
+# simple script to stand in for data manipulation with pandas
+# now it simply demonstration that docker is available in the container
+
+import pandas as pd
+
+# some fancy stuff with pandas
+
+print('job finished successfully')
