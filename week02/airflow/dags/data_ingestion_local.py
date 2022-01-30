ff1ee6d0c5bb6291c53ae3160bf33d30ac3130ff
Michael Harty <michaelharty3@gmail.com>
2022-01-30 04:36:17 +0000

    edit task to download a file from a url

--- week02/airflow/dags/data_ingestion_local.py
+++ week02/airflow/dags/data_ingestion_local.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+import os
 
 from airflow import DAG
 
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME")
+
 
 
 local_workflow = DAG(
@@ -12,16 +16,18 @@ local_workflow = DAG(
     start_date=datetime(2021, 1, 1)
 )
 
+url = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+
 with local_workflow:
 
     wget_task = BashOperator(
         task_id='wget',
-        bash_command='echo "hello world"'
+        bash_command=f'curl -sSL {url} > {AIRFLOW_HOME}/output.csv'
     )
 
     ingest_task = BashOperator(
         task_id='ingest',
-        bash_command='pwd'
+        bash_command=f'ls {AIRFLOW_HOME}'
     )
 
     wget_task >> ingest_task
