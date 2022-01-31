f23d20517fce4d78c197660fd3031e186820aa3f
Michael Harty <michaelharty3@gmail.com>
2022-01-31 03:20:16 +0000

    add dags for week 2 homework

--- week02/airflow/dags/data_ingestion_local.py
+++ week02/airflow/dags/data_ingestion_local.py
@@ -6,6 +6,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+# home directory inside our worker is stored as an environment variable
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME")
 
 
@@ -16,13 +17,18 @@ local_workflow = DAG(
     start_date=datetime(2021, 1, 1)
 )
 
-url = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+
+# Airflow uses jinja templating. and provides access to certain variables for instance
+# the execution_date which we use below along with strftime to convert to the proper format
+URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_'
+URL_TEMPLATE = URL_PREFIX + '{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\'%Y-%m\') }}.csv'
 
 with local_workflow:
 
     wget_task = BashOperator(
         task_id='wget',
-        bash_command=f'curl -sSL {url} > {AIRFLOW_HOME}/output.csv'
+        bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
     )
 
     ingest_task = BashOperator(
---
+++ week02/airflow/dags/fhv_data_ingestion_dag.py
@@ -0,0 +1,104 @@
+import os
+import logging
+from datetime import datetime
+
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from google.cloud import storage
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+
+DATASET_FILE_PREFIX = "fhv_tripdata_"
+DATASET_FILE_TEMPLATE = DATASET_FILE_PREFIX + '{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+DATASET_URL = f"https://nyc-tlc.s3.amazonaws.com/trip+data/{DATASET_FILE_TEMPLATE}"
+PARQUET_FILE = DATASET_FILE_TEMPLATE.replace('.csv', '.parquet')
+
+
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+    table = pv.read_csv(src_file)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
+
+# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="fhv_ingestion_dag",
+    start_date=datetime(2019, 1, 1),
+    schedule_interval="@monthly",
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['dtc-de'],
+) as dag:
+
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"curl -sSfL {DATASET_URL} > {AIRFLOW_HOME}/{DATASET_FILE_TEMPLATE}"
+    )
+
+    format_to_parquet_task = PythonOperator(
+        task_id="format_to_parquet_task",
+        python_callable=format_to_parquet,
+        op_kwargs={
+            "src_file": f"{AIRFLOW_HOME}/{DATASET_FILE_TEMPLATE}",
+        },
+    )
+
+    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{PARQUET_FILE}",
+            "local_file": f"{AIRFLOW_HOME}/{PARQUET_FILE}",
+        },
+    )
+
+    cleanup_task = BashOperator(
+        task_id="cleanup_task",
+        bash_command=f'rm {AIRFLOW_HOME}/{DATASET_FILE_TEMPLATE} {AIRFLOW_HOME}/{PARQUET_FILE}'
+    )
+
+download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> cleanup_task
